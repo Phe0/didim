4888b0b9688cd14be40472f3c6d0b87a8eb80d60
phe0 <pheofo@gmail.com>
2024-07-23 18:22:37 -0300

    remove unnecessary logic and pages

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,8 +1,3 @@
-import { currentUser } from "@clerk/nextjs/server";
-import { getUserConfig } from "../api/user-config";
-
 export default async function OnboardingPage() {
-  const userConfig = await getUserConfig();
-
   return <h1>Onboarding</h1>;
 }
