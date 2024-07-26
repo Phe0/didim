04fd4aa49450df2096fe01c39cf6c18b3fd74abf
phe0 <pheofo@gmail.com>
2024-07-26 15:53:23 -0300

    fix entity imports

--- domain/entities/__tests__/fraction.test.ts
+++ domain/entities/__tests__/fraction.test.ts
@@ -1,34 +1,34 @@
-import { Fraction } from "../../fraction";
+import { Fraction } from "../fraction";
 
 describe("Fraction", () => {
   it("should instantiate a fraction successfully", () => {
     const fraction = new Fraction({
-      id: 1,
+      id: "1",
       name: "Fraction 1",
-      percentage: 0.5,
+      percentage: 50,
       profileId: "123",
     });
 
     expect(fraction).toBeInstanceOf(Fraction);
-    expect(fraction.id).toBe(1);
+    expect(fraction.id).toBe("1");
     expect(fraction.name).toBe("Fraction 1");
-    expect(fraction.percentage).toBe(0.5);
+    expect(fraction.percentage).toBe(50);
     expect(fraction.profileId).toBe("123");
   });
 
   it("should correctly convert to insert object", () => {
     const fraction = new Fraction({
-      id: 1,
+      id: "1",
       name: "Fraction 1",
-      percentage: 0.5,
+      percentage: 50,
       profileId: "123",
     });
     const insert = fraction.toInsert();
 
     expect(insert).toEqual({
-      id: 1,
+      id: "1",
       name: "Fraction 1",
-      percentage: "0.5",
+      percentage: "50",
       profileId: "123",
     });
   });
